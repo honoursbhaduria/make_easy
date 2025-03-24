8cff5e67303fbb3e514bd0d0334d893c92c3e436
honoursbhaduria <honoursbadauria@gmail.com>
2025-03-24 19:26:38 +0530

    update

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -9,6 +9,18 @@ def index (request):
 
 
 def user_login(request):
+    if request.method == 'POST' :
+        username = request.POST['username']
+        createpassword =  request.POST['createpassword']
+
+        user = authenticate(request , username=username , password = createpassword)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = "Invalid username or password"
+            return render(request, 'login.html',{'error_message'})
+
     return render (request , 'login.html')
 
 def user_signup (request):
