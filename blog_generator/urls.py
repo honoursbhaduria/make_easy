fd99df6f3c6ec28378ebef672d58b54ff76c52f9
honoursbhaduria <honoursbadauria@gmail.com>
2025-03-25 21:49:32 +0530

    update

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/',views.user_login , name ='login'),
     path('signup/',views.user_signup , name ='signup'),
     path('logout/',views.user_logout , name ='logout'),
+    path('generate-blog/',views.generate_blog , name ='generate-blog'),
 
 
 ]
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -2,13 +2,25 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate ,login , logout
 from django.shortcuts import render ,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required (login_url="/login")
 def index (request):
     return render (request , 'index.html')
 
+#or i can you this to 
 
+# def index(request):
+#     if not request.user.is_authenticated:
+#         return redirect("/login")
+#     return render(request, "index.html")
+
+
+def generate_blog(request):
+     pass
+     
 def user_login(request):
     error_message = ""
     username = ""  
@@ -20,9 +32,14 @@ def user_login(request):
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")  # Avoid KeyError
     
-    if not username or not password:
-            error_message = "Username and password are required."
+    if not username :
+            error_message = "Username incarrect "
             return render(request, "login.html")
+    
+    if not password:
+            error_message = "check your password it's incarrect "
+            return render(request, "login.html")
+
 
     user = authenticate(request, username=username, password=password)
     if user is not None:
